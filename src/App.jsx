b3b06478baa26fb9b7d536c79906f2dde001acdd
DeveloperMokchhedul <mokchheduls46@gmail.com>
2024-12-15 06:36:37 +0600

    shadcn connect with project successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
     <div>
      <h1 className='text-2xl bg-red-600'> welcome to restaurant website</h1>
+     <Button>shadcnbtn</Button>
     </div>
   )
 }
